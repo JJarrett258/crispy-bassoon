99acb0afe2642f56b50bf86fdcc97f73fe63b501
Jonathan Jarrett <jonathan.jarrett@autotrader.co.uk>
2020-12-04 16:18:31 +0000

    initial commit - day one complete

---
+++ 1/solution.ts
@@ -0,0 +1,47 @@
+import { inputData } from './data'
+
+let inputNumbers = inputData
+const targetNum = 2020
+let operands: number[] = []
+
+const twoOperandsSolutionFinder = (): void => {
+        let num1 = inputNumbers[0]
+        for (let i = 1; i < inputNumbers.length; i++) {
+                let num2 = inputNumbers[i]
+                if (num1 + num2 === targetNum) {
+                        console.log (
+                                `Matches: ${num1} ${num2}\nsolution: ${num1 * num2}`
+                        )
+                        return
+                }
+        }
+        inputNumbers.shift()
+        twoOperandsSolutionFinder()
+}
+//twoOperandsSolutionFinder()
+
+const threeOperandsSolutionFinder = () => {
+
+        let num1 = inputNumbers[0]
+
+        for (let i = 1; i < inputNumbers.length; i++) {
+                let num2 = inputNumbers[i]
+
+                for (let l = 2; l < inputNumbers.length; l++) {
+
+                        let num3 = inputNumbers[l]
+                        if (num1 + num2 + num3 === targetNum) {
+                                console.log (
+                                        `Matches: ${num1} ${num2} ${num3}\nsolution: ${num1 * num2 * num3}`
+                                )
+                                return
+                        }
+
+                }
+
+        }
+        inputNumbers.shift()
+        threeOperandsSolutionFinder()
+}
+threeOperandsSolutionFinder()
+
