06f79a2ce22323865637ef43fb1073a2a506321c
Jonathan Jarrett <jonathan.jarrett@autotrader.co.uk>
2021-12-03 16:56:39 +0000

    remove js directory

--- web-app/js/AdventOfCode2018/2/2.js
+++
@@ -1,9 +0,0 @@
-//aabbbcdee
-export const determineFrequencies = (inputString) => {
-    const indivLetters = inputString.split('');
-    let frequencyTable = {};
-    for (const letter of indivLetters) {
-        frequencyTable[letter] = 0;
-    }
-    return frequencyTable;
-};
