f9b493849bb2550425f188f36339ed84fe3fb38d
Jonathan Jarrett <jonathan.jarrett@autotrader.co.uk>
2021-12-11 21:26:08 +0000

    Day three: diagnostics data part one solution

--- 2021/3/binary-diagnostics.test.ts
+++ 2021/3/binary-diagnostics.test.ts
@@ -1,14 +1,24 @@
-import {getEpsilonRate, getGammaRate} from "./binary-diagnostics";
+import {getGammaRate, getResult, invert, parseArray} from "./binary-diagnostics";
 
 describe('binary diagnostics tests', () => {
     const diagnosticsData = ['00100','11110','10110','10111','10101','01111','00111','11100','10000','11001','00010','01010']
 
-    xtest('calculates gamma rate', () => {
+    test('calculates gamma rate', () => {
         expect(getGammaRate(diagnosticsData, false)).toEqual([1,0,1,1,0])
     })
 
     test('calculates gamma rate from reversed diagnostic input data', () => {
         expect(getGammaRate(diagnosticsData, true)).toEqual([0,1,0,0,1])
+    })
+
+    test('the gamma rate number from array of binary numbers', () => {
+        const arrayData = getGammaRate(diagnosticsData, false)
+        expect(parseArray(arrayData)).toEqual(22)
+        expect(parseArray(invert(arrayData))).toEqual(9)
+    })
+
+    test('returns correct power consumption', () => {
+        expect(getResult(diagnosticsData)).toEqual(198)
     })
 
 })
--- 2021/3/binary-diagnostics.ts
+++ 2021/3/binary-diagnostics.ts
@@ -1,14 +1,25 @@
+import {parseTextToStringArr} from "../../textParser";
+const inputData = parseTextToStringArr('./diagnostics-data.txt')
 
-
-export const getGammaRate = (diagnosticsData: string[], isEpsilonRate: boolean = false): number[] => {
-    let totalZerosInEachColumns: number[] = [0, 0, 0, 0, 0]
+function getSumOfOnesInBinaryColumns(diagnosticsData: string[]): number[] {
+    let totalZerosInEachColumns: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     diagnosticsData.forEach(entry => {
         for (let i = 0; i < entry.length; i++) {
-            if (entry.charAt(i) === '0') {
+            if (entry.charAt(i) === '1') {
                 totalZerosInEachColumns[i] += 1
             }
         }
     })
+    return totalZerosInEachColumns
+}
+
+export const getOxygenRating = (mostFrequentBitInDiagnosticData: number[], diagnosticData: string[]) => {
+
+
+}
+
+export const getGammaRate = (diagnosticsData: string[], isEpsilonRate: boolean = false): number[] => {
+    let totalZerosInEachColumns = getSumOfOnesInBinaryColumns(diagnosticsData)
     let result: number [] = []
     totalZerosInEachColumns.forEach(entry => {
         if (entry > diagnosticsData.length / 2) {
@@ -20,13 +31,19 @@ export const getGammaRate = (diagnosticsData: string[], isEpsilonRate: boolean =
     return isEpsilonRate === false ? result : invert(result)
 }
 
-export const getEpsilonRate = (diagnosticsData: string[]): number => {
-    return 1
-}
-
-function invert(binaryDigits: number[]) {
+export function invert(binaryDigits: number[]) {
     return binaryDigits.map(bit => bit === 0 ? 1 : 0)
 }
 
-function binaryArrayToNumber(binaryDigits: number[]) {
+export const parseArray = arr => {
+    return arr.reduce((acc, val) => {
+        return (acc << 1) | val;
+    });
 }
+
+export const getResult = (diagnosticData: string[]): number => {
+    const gammaRate = getGammaRate(diagnosticData)
+    const epsilonRate = invert(gammaRate)
+    return parseArray(gammaRate) * parseArray(epsilonRate)
+}
+
