19d72ec023f5c62633cfe468f7d7eea01cfeef98
Jonathan Jarrett <Jonathan.Jarrett@autotrader.co.uk>
2022-07-02 14:39:03 +0100

    Day 5: add new puzzle input and tidy up existing attempt

--- 2021/5/hydrothermal-vents.ts
+++ 2021/5/hydrothermal-vents.ts
@@ -1,11 +1,9 @@
 let things = [[]]
-for(let i: number = 0; i < 100; i++) {
+for(let i: number = 0; i < 10; i++) {
     things[i] = [i];
-    for(let j: number = 0; j< 100; j++) {
+    for(let j: number = 0; j < 10; j++) {
         things[i][j] = [0][0]
     }
 }
-// console.table(things)
-import * as fs from "fs";
-const myConsole = new console.Console(fs.createWriteStream('./output.txt'))
-myConsole.log('hello world')
+console.table(things)
+
--- spotTheDifference.ts
+++
@@ -1,7 +0,0 @@
-// this.allowEditStartDateTimeAndName = this.createMode || this.futureCampaign;
-
-let allowEditStartDateTimeAndName: boolean = true
-let futureCampaign: boolean = false
-
-function logic(bool1: boolean, bool1)
-
